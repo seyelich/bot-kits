10f70a25e8394cb19e729f7bfcb3f307796f1cbc
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-07-29 04:00:37 +0500

    just started

---
+++ src/components/sidebar/icons/dashboard-icon.tsx
@@ -0,0 +1,13 @@
+const dashboardIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+    <path
+      stroke="#CCD4E0"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M10 3H3v9h7V3ZM21 3h-7v5h7V3ZM21 12h-7v9h7v-9ZM10 16H3v5h7v-5Z"
+    />
+  </svg>
+);
+
+export default dashboardIcon;
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,12 +2,25 @@ import { NavLink } from 'react-router-dom';
 
 import styles from './sidebar.module.css';
 
+const addButton = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="none">
+    <circle cx="23" cy="23" r="23" fill="#243CBB" />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M23 17v12M17 23h12"
+    />
+  </svg>
+);
+
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <span>bot kits</span>
       <nav className={styles.navigation}>
-        <NavLink to="/add-bot">кнопошка добавить</NavLink>
+        <NavLink to="/add-bot">{addButton}кнопошка добавить</NavLink>
         <NavLink to="/">Дашборд</NavLink>
         <NavLink to="/bot-builder">Воронки</NavLink>
         <NavLink to="/mailing">Рассылки</NavLink>
