de38e5dad9922a6df272a5454e04d8a9cf8420a8
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-08-01 15:05:56 +0300

    сверстать верхний блок компонента с иконками

--- src/components/auth-form/AuthForm.tsx
+++ src/components/auth-form/AuthForm.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import FacebookIcon from '../../icons/social/facebook';
+import GoogleIcon from '../../icons/social/google';
+import MailruIcon from '../../icons/social/mailru';
+import OdnoklassnikiIcon from '../../icons/social/odnoklassniki';
+import TelegramIcon from '../../icons/social/telegram';
+import VKIcon from '../../icons/social/vk';
+import YandexIcon from '../../icons/social/yandex';
 import { Button } from '../Button/Button';
 import styles from './AuthForm.module.css';
 
@@ -41,46 +48,104 @@ const AuthForm = () => {
   return (
     <div className={styles.authForm}>
       <div className={styles.authForm__socialContainer}>
-        <p className={styles.authForm__socialContainer__title}>Создай аккаунт с помощью</p>
-        </div>
-      <form className={styles.authForm__form} onSubmit={handleSubmit}>
-        <div className={styles.authForm__inputContainer}>
-          <input className={styles.authForm__defautInput} type="text" placeholder="Имя" />
-          <input type="email" placeholder="Email" />
-          <div className={styles.authForm__passwordContainer}>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Пароль"
-            />
-            <button type="button" onClick={handlePasswordVisibility}>
-              {showPassword ? 'Скрыть' : 'Показать'}
-            </button>
-          </div>
+        <p className={styles.authForm__socialContainer__title}>
+          Создай аккаунт с помощью
+        </p>
+        <div className={styles.authForm__socialContainer__menuContainer}>
+          <nav className={styles.authForm__socialContainer__menu}>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <GoogleIcon width={40} height={40} type="blue" />
+            </NavLink>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <YandexIcon width={40} height={40} type="blue" />
+            </NavLink>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <MailruIcon width={40} height={40} type="blue" />
+            </NavLink>
+          </nav>
+          <nav className={styles.authForm__socialContainer__menu}>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <VKIcon width={40} height={40} type="blue"/>
+            </NavLink>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <OdnoklassnikiIcon width={40} height={40} type="blue"/>
 
-          <div className={styles.authForm__phoneContainer}>
-            <select value={countryCode} onChange={handleCountryCodeChange}>
-              {options}
-              {/* <option value="+375">+375 (Беларусь)</option>
-            <option value="+7">+7 (Россия)</option> */}
-            </select>
+            </NavLink>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <FacebookIcon width={40} height={40} type="blue"/>
+
+            </NavLink>
+            <NavLink
+              className={styles.authForm__socialContainer__menuItem}
+              to="#"
+            >
+              <TelegramIcon width={40} height={40} type="blue"/>
+            </NavLink>
+          </nav>
+        </div>
+      </div>
+      <div className={styles.authForm__wrapper}>
+        <form className={styles.authForm__form} onSubmit={handleSubmit}>
+          <div className={styles.authForm__inputContainer}>
             <input
+              className={styles.authForm__defautInput}
               type="text"
-              placeholder="Телефон"
-              value={phoneNumber}
-              onChange={handlePhoneNumberChange}
+              placeholder="Имя"
             />
-          </div>
-        </div>
-        <Button type="green" text="СОЗДАТЬ АККАУНТ" width={260} height={64} />
-      </form>
+            <input type="email" placeholder="Email" />
+            <div className={styles.authForm__passwordContainer}>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Пароль"
+              />
+              <button type="button" onClick={handlePasswordVisibility}>
+                {showPassword ? 'Скрыть' : 'Показать'}
+              </button>
+            </div>
 
-      <div className={styles.authForm__footer}>
-        <span className={styles.authForm__footer__text}>
-          Уже прошли регистрацию?
-        </span>
-        <NavLink className={styles.authForm__footer__link} to="#">
-          Войти
-        </NavLink>
+            <div className={styles.authForm__phoneContainer}>
+              <select value={countryCode} onChange={handleCountryCodeChange}>
+                {options}
+                {/* <option value="+375">+375 (Беларусь)</option>
+            <option value="+7">+7 (Россия)</option> */}
+              </select>
+              <input
+                type="text"
+                placeholder="Телефон"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+              />
+            </div>
+          </div>
+          <Button type="green" text="СОЗДАТЬ АККАУНТ" width={260} height={64} />
+        </form>
+
+        <div className={styles.authForm__footer}>
+          <span className={styles.authForm__footer__text}>
+            Уже прошли регистрацию?
+          </span>
+          <NavLink className={styles.authForm__footer__link} to="#">
+            Войти
+          </NavLink>
+        </div>
       </div>
     </div>
   );
