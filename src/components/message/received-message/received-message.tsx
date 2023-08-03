fddd4890342895a62e37a7844a1f375e0a921296
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-08-04 00:56:03 +0300

    Фикс пропсов компонента message

--- src/components/message/received-message/received-message.tsx
+++ src/components/message/received-message/received-message.tsx
@@ -2,35 +2,41 @@ import React from 'react';
 import styles from './received-message.module.css';
 import ChatAvatarReceived from '../../../icons/others/ChatAvatarReceived';
 
-function ReceivedMessage({
-  message = 'Привет, как это сделать?',
-  sendTime = '16 мин назад',
-  status = {
-    time: '14:54',
-    delivered: true,
-  },
-  from = 'Сергей Надеин',
-}: {
-  message?: string;
-  sendTime?: string;
-  status?: {
-    time: string;
-    delivered: boolean;
+type TReceivedMessage = {
+  message: {
+    text: string;
+    sendTime: string;
+    status: {
+      time: string;
+      delivered: boolean;
+    };
+    from: string;
   };
-  from?: string;
-}) {
+};
+
+function ReceivedMessage({
+  message = {
+    text: 'Привет, как это сделать?',
+    sendTime: '16 мин назад',
+    status: {
+      time: '14:54',
+      delivered: true,
+    },
+    from: 'Сергей Надеин',
+  },
+}: TReceivedMessage) {
   return (
     <div className={styles.block}>
       <div className={styles.container}>
-        <p className={styles.name}>{from}</p>
-        <p className={styles.message}>{message}</p>
-        <p className={styles.sendTime}>{sendTime}</p>
+        <p className={styles.name}>{message.from}</p>
+        <p className={styles.message}>{message.text}</p>
+        <p className={styles.sendTime}>{message.sendTime}</p>
       </div>
       <ChatAvatarReceived />
       <p className={styles.status}>
-        {status && status.delivered
-          ? `Просмотрено в ${status!.time}`
-          : `Отправлено в ${status!.time}`}
+        {message.status.delivered
+          ? `Просмотрено в ${message.status.time}`
+          : `Отправлено в ${message.status.time}`}
       </p>
     </div>
   );
--- src/components/message/sent-message/sent-message.tsx
+++ src/components/message/sent-message/sent-message.tsx
@@ -1,34 +1,40 @@
 import ChatAvatarSent from '../../../icons/others/ChatAvatarSent';
 import styles from './sent-message.module.css';
 
+type TSendMessage = {
+  message: {
+    text: string;
+    sendTime: string;
+    status: {
+      time: string;
+      delivered: boolean;
+    };
+  };
+};
+
 // Дефолтные значения пропсов нужны для отладки верстки компонента.
 function SentMessage({
-  message = 'Привет, Перейдите по ссылке https://BOTkit/file/-node-id=3A332705',
-  sendTime = '10 мин назад',
-  status = {
-    time: '14:54',
-    delivered: true,
+  message = {
+    text: 'Привет, Перейдите по ссылке https://BOTkit/file/-node-id=3A332705',
+    sendTime: '10 мин назад',
+    status: {
+      time: '14:54',
+      delivered: true,
+    },
   },
-}: {
-  message?: string;
-  sendTime?: string;
-  status?: {
-    time: string;
-    delivered: boolean;
-  };
-}) {
+}: TSendMessage) {
   return (
     <div className={styles.block}>
       <div className={styles.container}>
         <p className={styles.name}>Вы</p>
-        <p className={styles.message}>{message}</p>
-        <p className={styles.sendTime}>{sendTime}</p>
+        <p className={styles.message}>{message.text}</p>
+        <p className={styles.sendTime}>{message.sendTime}</p>
       </div>
       <ChatAvatarSent />
       <p className={styles.status}>
-        {status && status.delivered
-          ? `Просмотрено в ${status!.time}`
-          : `Отправлено в ${status!.time}`}
+        {message.status.delivered
+          ? `Просмотрено в ${message.status.time}`
+          : `Отправлено в ${message.status.time}`}
       </p>
     </div>
   );
