0241db5b0aa70c004915fa0d4928b6eb3b54d24d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-08-04 03:17:34 +0300

    добавить часть схемы LoginForm

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -5,32 +5,32 @@ import LoginForm from '../../components/login-form/LoginForm';
 import { useState } from 'react';
 
 export default function Auth() {
+  const [state, setState] = useState({ authState: 'Login' });
 
-  const [state, setState] = useState({authState:"Register"})
+  const handleRegisterClick = () => {
+    setState({ authState: 'Register' });
+  };
 
-    const handleRegisterClick = () => {
-      setState({ authState: "Register" });
-    };
+  const handleLoginClick = () => {
+    setState({ authState: 'Login' });
+  };
 
-    const handleLoginClick = () => {
-      setState({ authState: "Login" });
-    };
-
-    const handleForgotPassClick = () => {
-      setState({ authState: "ForgotPass" });
-    };
-
-    console.log(state);
-
+  const handleForgotPassClick = () => {
+    setState({ authState: 'ForgotPass' });
+  };
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.headerContainer}>
         <Logo register={true} />
-        <h1 className={styles.title}>Регистрация</h1>
+        <h1 className={styles.title}>
+          {state.authState === 'Register' && 'Регистрация'}
+          {state.authState === 'Login' && 'Вход'}
+          {state.authState === 'ForgotPass' && 'Восстановление пароля'}
+        </h1>
       </div>
-      <RegisterForm logIn={handleLoginClick}/>
-      {/* <LoginForm/> */}
+      {/* <RegisterForm logIn={handleLoginClick}/> */}
+      <LoginForm />
     </div>
   );
 }
