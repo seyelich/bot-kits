fdf1a446fb068d93ee6e3f8293c118dddcc805e5
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-08-04 10:55:25 +0300

    изменить ссылку на Dashbord

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/sidebar/sidebar';
-import Dashboard from './pages/dashboard';
+import Dashboard from './pages/Dashboard/dashboard';
 import AddBot from './pages/add-bot';
 import styles from './App.module.css';
 import BotBuilder from './pages/bot-builder';
