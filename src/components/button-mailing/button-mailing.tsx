a39f11ddd16d94794842ffc629b19cf0b247c989
kglidiya <betysmith@yandex.ru>
2023-08-05 11:50:20 +0300

    minor fixes

--- src/components/button-mailing/button-mailing.tsx
+++ src/components/button-mailing/button-mailing.tsx
@@ -13,26 +13,30 @@ const ButtonMailing: FC<IButtonMailing> = ({ width, text, setSelected }) => {
   return (
     <div className={styles.container}>
       <button
+        type="button"
         onClick={() => {
           setActive(!isActive);
-          if(text === 'Число месяца') {
-            setSelected(!isActive)
+          if (text === 'Число месяца') {
+            setSelected(!isActive);
           }
-          if(text === 'День месяца') {
-            setSelected(!isActive)
+          if (text === 'День месяца') {
+            setSelected(!isActive);
           }
-          if(text === 'Месяц отправки') {
-            setSelected(!isActive)
+          if (text === 'Месяц отправки') {
+            setSelected(!isActive);
           }
-          console.log(text)
-          console.log(setSelected)
         }}
         className={`${styles.input} `}
         style={{ width }}
       >
         {text}
       </button>
-      {!isActive && <span className={styles.button}> <CheckIcon color="#243CBB" width={16} height={16} /></span> }
+      {isActive && (
+        <span className={styles.button}>
+          {' '}
+          <CheckIcon color="#243CBB" width={16} height={16} />
+        </span>
+      )}
     </div>
   );
 };
