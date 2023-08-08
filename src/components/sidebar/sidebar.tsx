9ba94437c4b5355027ee5b74f294553fd67befc2
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-08-09 00:17:04 +0500

    add: изменил иконки сайдбара

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -79,11 +79,60 @@ export default function Sidebar() {
             <span className={styles.navtext}>Общий доступ</span>
           </NavLink>
           <NavLink to="/subscription" className={linkStyle}>
-            {shareIcon}
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18 16.7306V8.36841"
+                stroke="#CCD4E0"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M12 16.7306V3.35107"
+                stroke="#CCD4E0"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M2.73621 20.6489H21.2639"
+                stroke="#CCD4E0"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M6 16.7306V11.7133"
+                stroke="#CCD4E0"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
             <span className={styles.navtext}>Подписки и платежи</span>
           </NavLink>
           <NavLink to="/partnership" className={linkStyle}>
-            {shareIcon}
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 11L15.5 19M16 11C16 6.5 14.5 4 11.9999 2C9.5 4 8 6.5 8 11M16 11L18 12.5C19.259 13.4443 20 14.9262 20 16.5V19.4612C20 20.1849 19.2551 20.669 18.5939 20.375L15.5 19M8 11L8.5 19M8 11L6 12.5C4.74097 13.4443 4 14.9262 4 16.5V19.4612C4 20.1849 4.74485 20.669 5.40614 20.375L8.5 19M8.5 19H15.5M12 9V11"
+                stroke="#D7DEEA"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
             <span className={styles.navtext}>Партнёрская программа</span>
           </NavLink>
         </nav>
