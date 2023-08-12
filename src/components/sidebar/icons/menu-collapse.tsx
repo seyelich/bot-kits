aaa08391eddc507bfabead91c5a51cae3d075642
Александр Огородников <ogorodnikov.aaleksandr@yandex.ru>
2023-08-12 20:07:00 +0500

    удалил лишнее

--- src/components/sidebar/icons/menu-collapse.tsx
+++ src/components/sidebar/icons/menu-collapse.tsx
@@ -1,7 +1,6 @@
 const menuCollapseIcon = (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
     <path
-      // stroke="#060C23"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="2"
